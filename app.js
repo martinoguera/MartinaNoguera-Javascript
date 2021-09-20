72b9014a16e13dbea54099571c637b55759f83a2
martinoguera <martina_noguera2001@hotmail.com>
2021-09-20 00:42:13 -0300

    Primer commit

---
+++ app.js
@@ -0,0 +1,18 @@
+
+let nombre = prompt ("¿Cual es tu nombre?")
+
+console.log ("¡Hola Martina, bienvenida!")
+
+let edad = Number (prompt ("¿Que edad tienes?") );
+
+const numero = 10;
+
+console.log (edad + numero)
+console.log (edad - numero)
+console.log (edad / numero)
+console.log (edad * numero)
+
+let mensaje = prompt ("¿Que mensaje quieres dejar?")
+
+
+
