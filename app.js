9a6756215dd9dd951f296b63ae6ab495b7cdefea
martinoguera <martina_noguera2001@hotmail.com>
2021-09-27 15:05:18 -0300

    Entrega clase 3

--- app.js
+++ app.js
@@ -1,31 +1,18 @@
 
-const nombrePersona1 = "martina"
-const nombrePersona2 = "melina"
-const nombrePersona3 = "victoria"
 
-let nombreDinamico = prompt("¿Cual es tu nombre?")
+let num1;
+let num2;
 
-nombreDinamico = nombreDinamico.toLowerCase()
+do{
+    num1= Math.floor(Math.random() * (7 - 1)) + 1;
+
+    num2= Math.floor(Math.random() * (7 - 1)) + 1;
+
+    console.log(num1, num2);
+
+}while (num1 != num2);
 
 
-if(nombreDinamico === nombrePersona1 || nombreDinamico === nombrePersona2 || nombreDinamico === nombrePersona3) {
-    console.log(`¡Hola ${nombreDinamico}, bienvenida al Bar!`)
-
-    let reserva = Number(prompt("¿Cual es tu numero de reserva?"))
-
-    if(reserva < 3) {
-        console.log ("Correcto, tenes tu mesa en el piso 1")
-    } else if (reserva > 4){
-        console.log("Correcto, tenes tu mesa en el piso 2")
-    } else if (reserva == 4){
-        console.log("No tenemos ese numero de reserva")
-    } else {
-     console.log("Espera que te buscamos una mesa")
-    }
-
- } else{ 
-        console.log(`No podes ingresar ${nombreDinamico}, no te tengo en la lista!`)
-}
 
 
 
