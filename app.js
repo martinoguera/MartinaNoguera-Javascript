27017e2b07a09c5f1cf75a8eda9c1f7c7dca517f
martinoguera <martina_noguera2001@hotmail.com>
2021-10-13 17:18:39 -0300

    Tp DOM

--- app.js
+++ app.js
@@ -1,58 +1,92 @@
 
-let edad = Number(prompt("¿Que edad tienes?"));
+// let edad = Number(prompt("¿Que edad tienes?"));
 
-if(edad >= 18){
-    console.log ("Podes comprar en la tienda");
-} else if (edad <= 17){
-    alert ("No puedes comprar en la tienda");
-}
+// if(edad >= 18){
+//     console.log ("Podes comprar en la tienda");
+// } else if (edad <= 17){
+//     alert ("No puedes comprar en la tienda");
+// }
 
-class Producto{ 
+// class Producto{ 
 
-    constructor(nombre, precio){
-        this.nombre = nombre;
+//     constructor(nombre, precio){
+//         this.nombre = nombre;
+//         this.precio = precio;
+//     }
+// }
+
+// let listaProductos = [];
+
+// listaProductos.push(new Producto("Fernet", 650))
+// listaProductos.push(new Producto("Vodka", 685))
+// listaProductos.push(new Producto("Heineken", 264))
+// listaProductos.push(new Producto("Corona", 184))
+// listaProductos.push(new Producto("Ron", 700))
+// listaProductos.push(new Producto("Brahma", 164))
+// listaProductos.push(new Producto("Whisky", 1560))
+// listaProductos.push(new Producto("Campari", 600))
+// listaProductos.push(new Producto("Jaggermeister", 2400))
+
+// const comprarProductos = () => {
+
+//     let nombre = prompt("Nombre de la bebida");
+//     let precio = Number(prompt("Precio del producto"));
+
+//     let Producto = new Producto(nombre, precio)
+//     listaProductos.push(Producto)
+// }
+
+// listaProductos.sort((a,b) =>{
+
+//     if(a.nombre > b.nombre) {
+//         return 1
+//     }
+//     if (a.nombre < b.nombre){
+//         return -1
+//     }
+//     return 0
+// })
+
+// console.log(listaProductos);
+
+// listaProductos.forEach(obj =>{
+//     console.log(obj.precio);
+// })
+
+// let search = prompt("que queres buscar?")
+// let buscadoUnico = listaProductos.find(obj => obj.nombre == search)
+// console.log(buscadoUnico)
+
+
+// DOM
+
+class p {
+    constructor(bebida, cantidad, precio, subtotal){
+        this.bebida = bebida;
+        this.cantidad = cantidad;
         this.precio = precio;
+        this.subtotal = subtotal;
     }
 }
 
-let listaProductos = [];
+const guardarDatos = () => {
 
-listaProductos.push(new Producto("Fernet", 650))
-listaProductos.push(new Producto("Vodka", 685))
-listaProductos.push(new Producto("Heineken", 264))
-listaProductos.push(new Producto("Corona", 184))
-listaProductos.push(new Producto("Ron", 700))
-listaProductos.push(new Producto("Brahma", 164))
-listaProductos.push(new Producto("Whisky", 1560))
-listaProductos.push(new Producto("Campari", 600))
-listaProductos.push(new Producto("Jaggermeister", 2400))
+const producto = new p({
 
-const comprarProductos = () => {
+    bebida : document.getElementById("bebida").value, 
+    cantidad : document.getElementById("cantidad").value,
+    precio : document.getElementById("precio").value,
+    subtotal : document.getElementById("subtotal").value,
 
-    let nombre = prompt("Nombre de la bebida");
-    let precio = Number(prompt("Precio del producto"));
+})
+    console.log(document.getElementById("bebida").value)
+    console.log(document.getElementById("cantidad").value)
+    console.log(document.getElementById("precio").value)
+    console.log(document.getElementById("subtotal").value)
 
-    let Producto = new Producto(nombre, precio)
-    listaProductos.push(Producto)
+    return producto
 }
 
-listaProductos.sort((a,b) =>{
-
-    if(a.nombre > b.nombre) {
-        return 1
-    }
-    if (a.nombre < b.nombre){
-        return -1
-    }
-    return 0
+document.getElementById("btn").addEventListener("click", () => {
+    guardarDatos()
 })
-
-console.log(listaProductos);
-
-listaProductos.forEach(obj =>{
-    console.log(obj.precio);
-})
-
-let search = prompt("que queres buscar?")
-let buscadoUnico = listaProductos.find(obj => obj.nombre == search)
-console.log(buscadoUnico)
