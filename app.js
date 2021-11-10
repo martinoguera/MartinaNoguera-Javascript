06848e69177441ea540c80c52147c54fa42bdb5d
martinoguera <martina_noguera2001@hotmail.com>
2021-11-10 17:52:20 -0300

    Proyecto final 1

--- app.js
+++ app.js
@@ -44,7 +44,7 @@
 //     lista.push(crearCarrito)
 //     return lista
 // }
-  
+
 
 // const guardarEnBaseDeDatos = () =>{
 
@@ -99,7 +99,7 @@
 //     <b> $ ${producto.precio}</b></div>`);
 // }
 
-
+/* 
 // onclick
 
 function myFunction() {
@@ -159,4 +159,107 @@ $.get(url, (respuesta, estado) =>{
 
         });
     }
+}) */
+/* 
+<div class="bottles col-sm col-md-6 col-xl-3">
+<img src="./imagenes/fernet.PNG" alt="" height="300px" width="300px">
+<div class="descripcion">
+  <div class="nombreProducto">Fernet Branca 750cc</div>
+  <div class="precio">$500</div>
+  <button class="shop" onclick="seleccionarProducto">AGREGAR</button>
+</div>
+</div>
+ */
+
+class Producto {
+  constructor({
+      id,
+      nombre,
+      precio,
+      destacado,
+      imagen
+  }) {
+      this.id = id;
+      this.nombre = nombre;
+      this.precio = precio;
+      this.destacado = destacado;
+      this.imagen = imagen;
+  }
+}
+
+const productos = [];
+
+const mostrarProductos = (place, array) => {
+  place.innerHTML = '';
+  array.forEach(element => {
+      place.innerHTML += `
+  <div class="bottles col-sm col-md-6 col-xl-3">
+      <img src="${element.imagen}" alt="" height="300px" width="300px">
+      <div class="descripcion">
+        <div class="nombreProducto">${element.nombre}</div>
+        <div class="precio">$${element.precio}</div>
+        <button class="shop" onclick="seleccionarProducto">AGREGAR</button>
+      </div>
+  </div>
+
+`;
+  });
+}
+
+const completarOptions = (place, array) => {
+  const fragment = document.createDocumentFragment();
+  array.forEach(element => {
+      const option = document.createElement('option');
+      option.setAttribute('value', element.id);
+      option.textContent = element.nombre;
+      fragment.appendChild(option);
+  });
+  place.appendChild(fragment);
+}
+
+const mostrarProducto = () => {
+  //Capturo los input
+  const precioInput = document.getElementById('precio');
+  const subtotal = document.getElementById('subtotal');
+  const bebida = document.getElementById('bebida');
+  const bebidaElegida = parseInt(bebida.options[bebida.selectedIndex].value);
+  const cantidad = parseInt(document.getElementById('cantidad').value);
+
+  //Busco el producto elegido y devuelvo el precio
+  const precio = productos.find(producto => producto.id == bebidaElegida).precio;
+
+  //Completo los input con los valores actualizados
+  precioInput.value = precio;
+  subtotal.value = precio * cantidad;
+}
+
+
+//Pido los datos de datos.json, los adapto a la clase Producto y los muestro por pantalla. Además hago el primer cálculo de la página en la sección de subtotal.
+$.get('./datos.json', (respuesta, estado) => {
+  if (estado == "success") {
+      //Ajusto los productos que llegan del archivo .json a la clase Producto
+      respuesta.forEach(producto => {
+          productos.push(new Producto({
+              id: producto.id,
+              nombre: producto.nombre,
+              precio: producto.precio,
+              destacado: producto.destacado,
+              imagen: producto.imagen,
+          }));
+      });
+      //capturo el contenedor donde imprimir los productos
+      const bottles = document.getElementById('bottles');
+      //Ejecuto la función para mostrar los productos en el contenedor bottles
+      mostrarProductos(bottles, productos);
+      //Completa los options de SUBTOTAL
+      completarOptions(bebida, productos);
+      //Muestra  el cálculo del primer producto
+      mostrarProducto();
+  }
 })
+
+
+//Evento para escuchar los cambios en el formulario y mostrar el precio por unidad y el subtotal  
+document.getElementById('formulario').addEventListener('change', () => {
+  mostrarProducto()
+});
